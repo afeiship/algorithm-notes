381bf0852777f1106406fe7bcde331578346488a
afeiship <1290657123@qq.com>
2018-01-22 16:50:16 +0800

    issue: quick sort revert

--- test/005-quickSort.js
+++ test/005-quickSort.js
@@ -7,9 +7,15 @@
 1.找基准值，设Pivot = a[0]
 2.分区（Partition）：比基准值小的放左边，大的放右边，基准值(Pivot)放左部与右部的之间。
 3.进行左部（a[0] - a[pivot-1]）的递归，以及右部（a[pivot+1] - a[n-1]）的递归，重复上述步骤。
+
+https://www.cnblogs.com/libin-1/p/5998870.html
+http://blog.csdn.net/yzllz001/article/details/50982841
  */
 
+var assert = require('assert');
 
+
+// 升序排列：
 function quickSort( inArray ){
 
   if( inArray.length <= 1) return inArray;
@@ -41,7 +47,51 @@ function quickSort( inArray ){
 }
 
 
-console.log(
+// 降序排列：
+function quickSort2( inArray ){
+  if( inArray.length <= 1) return inArray;
 
-  quickSort( [1,13,24,11,11,14,1,2] )
-);
+  var baseValue = inArray[0];
+  var left = [];
+  var right = [];
+
+  //这里要注意： 如果第一个已经作为base值了，那么就不能以 0 为起点了
+  for (let index = 1; index < inArray.length; index++) {
+    const item = inArray[index];
+    if( item > baseValue){
+      left.push( item );
+    }else{
+      right.push(item);
+    }
+  }
+
+  return [].concat(
+    //left:
+    quickSort2(left),
+
+    // middle:
+    [ baseValue ],
+
+    //right:
+    quickSort2(right)
+  );
+}
+
+// console.log(
+
+//   quickSort( [1,13,24,11,11,14,1,2] )
+// );
+
+
+describe('005-quickSort', function() {
+  it(" quickSort('result') ", function() {
+    var arr = [0, 1,13,24,11,14,2];
+    assert.deepEqual( quickSort(arr), [ 0, 1, 2, 11, 13, 14, 24 ] );
+  });
+
+
+  it(" quickSort2('reverse') ", function() {
+    var arr = [0, 1,13,24,11,14,2];
+    assert.deepEqual( quickSort2(arr), [ 24, 14, 13, 11, 2, 1, 0 ] );
+  });
+});
