db06e9b385ce4774abb5b3457e2aa736d7fab262
afeiship <1290657123@qq.com>
2018-01-23 10:27:40 +0800

    issue: get by className

---
+++ test/010-getElementsByClassName.js
@@ -0,0 +1,37 @@
+/**
+Q9 实现类似getElementsByClassName 的功能
+自己实现一个函数，查找某个DOM节点下面的包含某个class的所有DOM节点？不允许使用原生提供的 getElementsByClassName querySelectorAll 等原生提供DOM查找函数。
+
+https://www.cnblogs.com/libin-1/p/5998870.html
+
+关键点在这里： hasClass:
+return new RegExp('(?:^|\\s)' + className + '(?:\\s|$)').test(el.className);
+// 只需要定位位置，所以用: ?:
+// 两种情况：1. 开头位置，肯定没有空格等情况 2. 如果不是开头，就得加个空格来匹配，防止 这种情况导致错误 className="a" className="abc"
+
+
+
+return (' ' + el.className + ' ').indexOf(' ' + className + ' ') > -1 ;
+
+ */
+function getByElsClass(inClassName, inContext) {
+  // get by *
+  // node.className.indexOf( inClassName ) > -1;
+  // to array
+  var context = inContext || document;
+  var nodes = context.getElementsByTagName('*');
+  var length = nodes.length;
+  var BLANK = ' ';
+  var result = [ ];
+
+  for (var index = 0; index < length; index++) {
+    var el = nodes[index];
+    var className = el.className;
+    //hasClass
+    if( (BLANK + className + BLANK).indexOf(BLANK + inClassName + BLANK) > -1 ){
+      result.push( el );
+    }
+  }
+
+  return result;
+}
---
+++ test/011-BinarySearchTree.js
@@ -0,0 +1,16 @@
+/**
+Q10 使用JS 实现二叉查找树(Binary Search Tree)
+一般叫全部写完的概率比较少，但是重点考察你对它的理解和一些基本特点的实现。
+二叉查找树，也称二叉搜索树、有序二叉树（英语：ordered binary tree）是指一棵空树或者具有下列性质的二叉树：
+
+任意节点的左子树不空，则左子树上所有结点的值均小于它的根结点的值；
+任意节点的右子树不空，则右子树上所有结点的值均大于它的根结点的值；
+任意节点的左、右子树也分别为二叉查找树；
+没有键值相等的节点。二叉查找树相比于其他数据结构的优势在于查找、插入的时间复杂度较低。为O(log n)。二叉查找树是基础性数据结构，用于构建更为抽象的数据结构，如集合、multiset、关联数组等。
+https://www.cnblogs.com/libin-1/p/5998870.html
+https://segmentfault.com/a/1190000000740261
+http://blog.csdn.net/luckyxiaoqiang/article/details/7518888
+http://blog.csdn.net/qq_36048015/article/details/77899447
+
+https://www.imooc.com/learn/888
+ */
